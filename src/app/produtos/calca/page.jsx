405b5a96e1fcd583d9aaf1ae91746cc5b400fb2a
Vittutexx <vitortex06@gmail.com>
2023-10-20 20:21:55 -0300

    Atualização de todas as aulas.

---
+++ src/app/produtos/calca/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Calca(){
+    return(
+          
+            <div>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus ullam in ut, excepturi vel. Reprehenderit optio hic rerum nihil doloribus. Quos ipsam expedita veritatis aspernatur quasi, magnam eius odit.</p>
+                <p><Link href="/">HOME</Link></p>
+                <figure>
+                    <Image src="/img/clothes-615×640.png" alt="Calça" width={307} height={320}/>
+                </figure>
+            </div>
+    )
+}
---
+++ src/components/Cabecalho/cabecalho.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import './Cabecalho.scss'
+
+export default function Cabecalho() {
+  return (
+    <header className='cabecalho'>
+      <nav>
+        <Link href="/produtos/calca">CALÇA</Link>
+        <Link href="/produtos/camisa">CAMISA</Link>
+        <Link href="/produtos/meia">MEIA</Link>
+        <Link href="/produtos/tenis">TÊNIS</Link>
+        <Link href="/tabela/queijo">QUEIJOS</Link>
+      </nav>
+    </header>
+    
+  )
+}
